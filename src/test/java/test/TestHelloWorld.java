4457db112464f4e71345de5ed1250fb69b529b9a
mashuai@hudongpai.com <ms120190>
2017-09-21 17:51:19 +0800

    Sma: 创建项目框架: 目标实现功能: 1. 对接kafka,hbase,es,spark 2. 具体功能再讨论

---
+++ src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @Author: SMA
+ * @Date: 2017-09-21 17:48
+ * @Explain:
+ */
+public class TestHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

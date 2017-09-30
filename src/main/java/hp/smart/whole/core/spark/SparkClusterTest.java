1436c6f5c094f54aee7664e635c46b6b2df84fae
mashuai@hudongpai.com <ms120190>
2017-09-30 16:31:29 +0800

    Sma: 1. 测试Spark集群,standby模式

--- src/main/java/hp/smart/whole/core/spark/SparkClusterTest.java
+++ src/main/java/hp/smart/whole/core/spark/SparkClusterTest.java
@@ -25,7 +25,7 @@ public class SparkClusterTest implements Serializable {
     public SparkConf createSparkConf() {
         SparkConf sparkConf = new SparkConf().setAppName("SparkClusterTestAlone")
 //                .setMaster("local[2]")
-//                .setMaster("spark://smart-master:9099")
+                .setMaster("spark://smart-master:9099")
                 .set("spark.master.url", "spark://smart-master:9099")
                 .setJars(JavaSparkContext.jarOfClass(SparkClusterTest.class))
                 .set("spark.cores.max", "2")
